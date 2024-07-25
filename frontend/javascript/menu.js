ee222cf7210f5b2f52a02bd67d8226669695efb3
DanielMorillo <danni._@hotmail.es>
2024-07-24 21:03:37 -0300

    Actualizado

--- frontend/javascript/menu.js
+++ frontend/javascript/menu.js
@@ -8,47 +8,26 @@ $("ul").click(function(stop) {
 })
 
 
+//Cart-mobile
 
-//Ocultar/Mostrar
-
-/*const hideButton = document.getElementById('img-carrito');
-
-hideButton.addEventListener('click', () => {
-    let hideCarrito = document.getElementById('cart');
-    let currentDisplay = hideCarrito.style.display;
-
-    if (currentDisplay === 'none') {
-        hideCarrito.style.display = 'block'
-    } else {
-        hideCarrito.style.display = 'none';
-    }
-
-    
-})*/
-
-$(function(){
-    //oculta al hacer click fuera del elemento
-    
-    $(document).on("click",function(e) {
-       var hideCarrito = $("#cart");       
-             
-            if (!hideCarrito.is(e.target) && hideCarrito.has(e.target).length === 0) 
-               
-                { 
-            //Se ha pulsado en cualquier lado fuera de los elementos contenidos en la variable cart
-            hideCarrito.slideUp();               
-                           
-            }
-     });
-
-    //Muestra elemento 
-    $("#img-carrito").on("click", function(event){
-        $("#cart").slideToggle();
-        event.stopImmediatePropagation();
+$(document).ready(function () {
+    $(window).resize(function () {
+        if ($(Window).width() <= 768) {
+            $(function(){
+                $(document).on("click",function(e) {
+                   var hideCarrito = $("#cart");       
+                         if (!hideCarrito.is(e.target) && hideCarrito.has(e.target).length === 0) { 
+                        hideCarrito.slideUp();                            
+                        }
+                 });
+            
+                $("#img-carrito").on("click", function(event){
+                    $("#cart").slideToggle();
+                    event.stopImmediatePropagation();
+                });
+            });
+        } 
     });
-
-
 });
-
 
 
