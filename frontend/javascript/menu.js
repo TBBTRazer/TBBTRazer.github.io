4a801c29d869b5f105d00436aa9bbc6b953239af
DanielMorillo <danni._@hotmail.es>
2024-07-25 03:29:35 -0300

    Actualizado

--- frontend/javascript/menu.js
+++ frontend/javascript/menu.js
@@ -1,11 +1,29 @@
+//Menu-hamburguer
+/*
 $(".submenu").click(function() {
     $(this).children("ul").slideToggle();
-})
+});
 
 $("ul").click(function(stop) {
     stop.stopPropagation();
-    
-})
+});
+*/
+$(document).ready(function() {
+    // Ocultar los submenús inmediatamente después de que se cargue el documento
+    $(".submenu ul").hide();
+
+    // Agregar el evento de click a los elementos .submenu
+    $(".submenu").click(function() {
+        // Desplegar o ocultar el submenú correspondiente con una transición
+        $(this).children("ul").slideToggle('fast'); // Puedes ajustar la velocidad ('fast', 'slow', o un número en milisegundos)
+    });
+
+    // Evitar que el clic en los elementos ul propague hacia arriba
+    $("ul").click(function(stop) {
+        stop.stopPropagation();
+    });
+});
+
 
 
 //Cart-mobile
