2ebafb5ed1339931b540f68e2e1a3295f096fba7
DanielMorillo <danni._@hotmail.es>
2024-07-25 04:06:26 -0300

    Actualizado

--- frontend/javascript/menu.js
+++ frontend/javascript/menu.js
@@ -1,28 +1,38 @@
 //Menu-hamburguer
-/*
-$(".submenu").click(function() {
-    $(this).children("ul").slideToggle();
-});
-
-$("ul").click(function(stop) {
-    stop.stopPropagation();
-});
-*/
 $(document).ready(function() {
-    // Ocultar los submenús inmediatamente después de que se cargue el documento
-    $(".submenu ul").hide();
+    function checkWindowSize() {
+        if ($(Window).width() <= 768) {
+            // Ocultar los submenús inmediatamente después de que se cargue el documento
+            $(".submenu ul").hide();
 
-    // Agregar el evento de click a los elementos .submenu
-    $(".submenu").click(function() {
-        // Desplegar o ocultar el submenú correspondiente con una transición
-        $(this).children("ul").slideToggle('fast'); // Puedes ajustar la velocidad ('fast', 'slow', o un número en milisegundos)
-    });
+            // Agregar el evento de clic a los elementos .submenu
+            $(".submenu").click(function() {
+                // Desplegar o ocultar el submenú correspondiente con una transición
+                $(this).children("ul").slideToggle('slow'); // Puedes ajustar la velocidad ('fast', 'slow', o un número en milisegundos)
+            });
 
-    // Evitar que el clic en los elementos ul propague hacia arriba
-    $("ul").click(function(stop) {
-        stop.stopPropagation();
-    });
+            // Evitar que el clic en los elementos ul propague hacia arriba
+            $("ul").click(function(stop) {
+                stop.stopPropagation();
+            });
+
+            // Cerrar todos los submenús si se hace clic fuera del contenedor #nav-ul
+            $(document).click(function(event) {
+                if (!$(event.target).closest('#nav-ul').length) {
+                    $(".submenu ul").slideUp('slow');
+                }
+            });
+        } else {
+            // Si el tamaño de la ventana es mayor que 768px, no hacemos nada
+            // Aquí podrías revertir cualquier acción que hayas realizado para tamaños menores a 768px, si es necesario
+        }
+    }
+
+    // Llamar a la función checkWindowSize al cargar la página y al redimensionar la ventana
+    checkWindowSize();
+    $(Window).resize(checkWindowSize);
 });
+
 
 
 
