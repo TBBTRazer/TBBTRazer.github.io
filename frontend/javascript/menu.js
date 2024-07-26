ef2b0239bd3618af2ee8dd764ecb29d8ce43c646
DanielMorillo <danni._@hotmail.es>
2024-07-25 21:49:01 -0300

    Actualizado

--- frontend/javascript/menu.js
+++ frontend/javascript/menu.js
@@ -24,12 +24,42 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function toggleCart() {
     var cart = document.getElementById('cart');
+    var cartButton = document.getElementById('img-carrito'); // Supongamos que 'cart-button' es el botón o elemento que abre el carrito
+
+    // Mostrar el carrito al hacer clic en el botón
     if (cart.style.display === '' || cart.style.display === 'none') {
         cart.style.display = 'block';
+
+        // Agregar un event listener al documento para cerrar el carrito al hacer clic fuera de él
+        document.addEventListener('click', closeCartOutsideClick);
     } else {
         cart.style.display = 'none';
+
+        // Remover el event listener cuando se cierra el carrito
+        document.removeEventListener('click', closeCartOutsideClick);
+    }
+
+    // Función para cerrar el carrito si se hace clic fuera de él
+    function closeCartOutsideClick(event) {
+        // Verificar si el clic no fue dentro del carrito ni en el botón que lo abre
+        if (!cart.contains(event.target) && event.target !== cartButton && !event.target.closest('.tarjeta-producto')) {
+            cart.style.display = 'none';
+            document.removeEventListener('click', closeCartOutsideClick);
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
